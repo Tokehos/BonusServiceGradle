683149265136964501bcb08eb761319acead7bc9
TokeH <max-lukich@mail.ru>
2021-11-28 19:54:35 +0300

    add Junit vintage

--- src/test/java/ru/netology/BonusServiceTest.java
+++ src/test/java/ru/netology/BonusServiceTest.java
@@ -17,14 +17,6 @@ public class BonusServiceTest {
     }
 
     @Test
-    public void shouldLeftToByeForCashback1000() {
-        int actual = bonusService.remain(1000);
-        int expected = 0;
-
-        assertEquals(actual, expected);
-    }
-
-    @Test
     public void shouldLeftToByeForCashback1001() {
         int actual = bonusService.remain(1001);
         int expected = 999;
