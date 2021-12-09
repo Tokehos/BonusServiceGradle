10372263e87b76e11e2bc97d707c13957ae148cc
Tokehos <88163971+Tokehos@users.noreply.github.com>
2021-12-09 12:39:26 +0300

    Update BonusServiceTest.java

--- src/test/java/ru/netology/BonusServiceTest.java
+++ src/test/java/ru/netology/BonusServiceTest.java
@@ -29,9 +29,9 @@ public class BonusServiceTest {
     }
 
     @Test
-    public void shouldLeftToByeForCashback0() {
-        int actual = bonusService.remain(0);
-        int expected = 1000;
+    public void shouldLeftToByeForCashback1000() {
+        int actual = bonusService.remain(1000);
+        int expected = 0;
         assertEquals(expected, actual);
     }
 
